8c8cac66dfb6965dc11a0977d9f7a13d139716b2
chriskinal <118364224+chriskinal@users.noreply.github.com>
2024-05-09 11:03:32 -0500

    Add ethernet.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,37 @@
 #include <Arduino.h>
 #include "Adafruit_BNO08x_RVC.h"
+#include <NativeEthernet.h>
+#include <NativeEthernetUdp.h>
+
+struct ConfigIP
+{
+  uint8_t ipOne = 192;
+  uint8_t ipTwo = 168;
+  uint8_t ipThree = 5;
+};
+
+ConfigIP networkAddress; // 3 bytes
+
+// IP & MAC address of this module of this module
+byte Eth_myip[4] = {0, 0, 0, 0}; // This is now set via AgIO
+byte mac[] = {0x00, 0x00, 0x56, 0x00, 0x00, 0x78};
+
+unsigned int portMy = 5120;           // port of this module
+unsigned int AOGNtripPort = 2233;     // port NTRIP data from AOG comes in
+unsigned int AOGAutoSteerPort = 8888; // port Autosteer data from AOG comes in
+unsigned int portDestination = 9999;  // Port of AOG that listens
+char Eth_NTRIP_packetBuffer[512];     // buffer for receiving ntrip data
+
+// An EthernetUDP instance to let us send and receive packets over UDP
+EthernetUDP Eth_udpPAOGI;     // Out port 5544
+EthernetUDP Eth_udpNtrip;     // In port 2233
+EthernetUDP Eth_udpAutoSteer; // In & Out Port 8888
+
+IPAddress Eth_ipDestination;
+
+void EthernetStart();
+void udpNtrip();
+void ReceiveUdp();
 
 #define SerialAOG Serial                //AgIO USB conection
 HardwareSerial* SerialGPS = &Serial7;   //UM982 com1
@@ -42,6 +74,9 @@ void setup() {
   SerialRVC->begin(baudAOG);
   SerialRVC->addMemoryForRead(RVCrxbuffer, serial_buffer_size);
   SerialRVC->addMemoryForWrite(RVCtxbuffer, serial_buffer_size);
+
+  Serial.println("\r\nStarting Ethernet...");
+  EthernetStart();
 
   if (!rvc.begin(SerialRVC))
   { // connect to the sensor over hardware serial
